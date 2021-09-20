a6e1b9a134d92b6856b8013b9bdd1e55322b7d27
codingfan <codingfan4226@gmail.com>
2021-09-20 11:42:51 +0900

    03-parameter

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {'data':'blog list'}
+@app.get('/blog')
+def index(limit=10,published=True, sort:Optional[str]=None):
+    # only get 10 published blogs
+    if published:
+        return {'data':f'{limit} published blogs from the db'}
+    else:
+        return {'data':f'{limit} blogs from the db'}
 
 @app.get('/blog/unpublished')
 def unpublished():
@@ -17,6 +22,8 @@ def show(id: int):
 
 
 @app.get('/blog/{id}/comments')
-def comments(id: int):
+def comments(id:int, limit:int=10):
     #fetch comments of blog with id = id
     return {'data': {'1', '2'}}
+
+
